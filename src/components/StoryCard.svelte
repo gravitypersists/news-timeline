b4723fe9dc9e5d32ea620e6485c23d01bc25e4ed
Michael Silveira <gravitypersists@gmail.com>
2021-08-30 15:43:46 -0400

    some rudimentary timeline card display

--- src/components/StoryCard.svelte
+++ src/components/StoryCard.svelte
@@ -1,6 +1,30 @@
 <script>
 	export let doc
   console.log(doc)
+
+  const thumbnail = doc.multimedia.find(x => x.crop_name === "thumbStandard")
 </script>
 
-<h2><a href={doc.web_url} target="_blank" >{doc.headline.main}</a></h2>
+<a href={doc.web_url} target="_blank">
+  {#if thumbnail}
+    <img src="http://nytimes.com/{thumbnail.url}" />
+  {/if}
+  <h2>
+    {doc.headline.main}
+  </h2>
+</a>
+
+
+<style>
+  a {
+    color: inherit;
+    display: block;
+    text-decoration: none;
+  }
+  img {
+    display: inline
+  }
+  h2 {
+    display: inline;
+  }
+</style>
--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,6 +1,6 @@
 <script>
-  import StoryCard from "./StoryCard.svelte";
-	export let data;
+  import StoryCard from "./StoryCard.svelte"
+	export let data
 </script>
 
 {#each data as doc, i}
