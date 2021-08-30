f662c27772c8e5edfd49fdea4832f17b37858b48
Michael Silveira <gravitypersists@gmail.com>
2021-08-30 14:26:23 -0400

    gut boilerplate, render simple title card for each story

---
+++ src/components/StoryCard.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let doc
+  console.log(doc)
+</script>
+
+<h2><a href={doc.web_url} target="_blank" >{doc.headline.main}</a></h2>
---
+++ src/components/Timeline.svelte
@@ -0,0 +1,8 @@
+<script>
+  import StoryCard from "./StoryCard.svelte";
+	export let data;
+</script>
+
+{#each data as doc, i}
+  <StoryCard doc={doc} />
+{/each}
